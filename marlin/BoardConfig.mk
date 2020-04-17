0a3b438f64a8145e1af999451f9ed6dd8ebabdab
TheGabrielHoward <j0s3ghoward@gmail.com>
2020-04-17 15:51:50 +0000

    marlin & sailfish: Delete BoardConfigLineage
    Signed-off-by: TheGabrielHoward <j0s3ghoward@gmail.com>

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -3,8 +3,8 @@
 # Product-specific compile-time definitions
 #
 
-# Allow Lineage config to override others
--include device/google/marlin/marlin/BoardConfigLineage.mk
+# Include Vendor BoardConfig
+-include vendor/google/marlin/BoardConfigVendor.mk
 
 TARGET_BOARD_PLATFORM := msm8996
 TARGET_BOOTLOADER_BOARD_NAME := marlin
